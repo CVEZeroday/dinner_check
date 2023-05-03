fce95020e7ad533983b936538466b4a968f74b3e
CVEzeroday <cvezeroday5754@gmail.com>
2023-05-03 23:44:35 +0900

    Deleted cmake template and main.h implemented

--- _deps/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /data/data/com.termux/files/home/dinner_check/_deps/googletest-src/googletest
-# Build directory: /data/data/com.termux/files/home/dinner_check/_deps/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
