de04af94d49b47098fc184a2876007414e3b7977
CVEzeroday <cvezeroday5754@gmail.com>
2023-05-04 15:45:17 +0900

    install.sh modified

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ rm -rf ./Dinner_Checker.egg-info
 rm -rf ./dist
 rm -rf ./build
 rm dinnerchecker.cpython*
+mkdir data
 echo "Compiling..."
 python3 setup.py build_ext --inplace
 echo "Installing..."
