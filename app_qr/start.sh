9514a11a3a1c72c8ee366643ca836aef2143086d
CVE Zeroday <cvezeroday5754@gmail.com>
2023-06-20 21:05:08 +0900

    Ver 1.0.1: Bug Fixed

--- app_qr/start.sh
+++ app_qr/start.sh
@@ -2,6 +2,8 @@ log_dir="./log"
 log_prefix="logfile_$(date +%Y%m%d_%H%M%S)"
 log_file="$log_dir/$log_prefix.log"
 
+echo "Copying students data from main app..."
+cp ../app/data . -r
 nohup python3 -u app_qr.py > "$log_file" 2>&1 & 
 
 echo "App started. Log file: $log_file"
